83b1edda928e6be2d2c2282071c03cb50a7dbaa1
Tunji Oyewole <t.oyewole23@outlook.com>
2021-07-13 16:57:35 -0400

    Add project files.

---
+++ VirtualJewelsInSafe/Safe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualJewelsInSafe
+{
+    class Safe
+    {
+        private string contents = "precious jewels";
+        private string safeCombination = "12345";
+
+        public string Open(string combination)
+        {
+            if (combination == safeCombination) return contents;
+            return "";
+        }
+
+        public void PickLock(Locksmith lockpicker)
+        {
+            lockpicker.Combination = safeCombination;
+        }
+    }
+}
---
+++ VirtualJewelsInSafe/SafeOwner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualJewelsInSafe
+{
+    class SafeOwner
+    {
+        private string valuables = "";
+        public void ReceiveContents(string safeContents)
+        {
+            valuables = safeContents;
+            Console.WriteLine($"Thank you for returning my {valuables}!");
+        }
+    }
+}
